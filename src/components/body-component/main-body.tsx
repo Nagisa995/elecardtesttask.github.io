3ad841945582bb5838f37d476db232b1d0a14705
Nagisa <miku195@mail.ru>
2022-08-11 23:44:08 +0700

    add draft loading screen

--- src/components/body-component/main-body.tsx
+++ src/components/body-component/main-body.tsx
@@ -1,13 +1,22 @@
 import { FC } from "react";
 import { generateImageURL } from "../../helpers/utils";
 import { useAppSelector } from "../../hooks/redux";
+import { Loader } from "../ui-components/loader";
 
 export const MainBody: FC = () => {
-  const { imageData } = useAppSelector((store) => store.appReducer);
+  const { imageData, isImageDataLoading } = useAppSelector(
+    (store) => store.appReducer
+  );
 
   const imageCollection = imageData.map((image) => (
     <img src={generateImageURL(image.image)} />
   ));
 
-  return <div className="timebody"> {imageCollection}</div>;
+  return (
+    <>
+      {(isImageDataLoading && <Loader />) || (
+        <div className="timebody"> {imageCollection}</div>
+      )}
+    </>
+  );
 };
